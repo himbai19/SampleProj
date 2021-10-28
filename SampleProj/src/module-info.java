506d8cf5eab50ff16d27a45dbace0580429c4241
Himanshu Bai <73933168+himbai19@users.noreply.github.com>
2021-10-28 07:54:13 +0530

    Updated the message to Hello India

--- SampleProj/src/module-info.java
+++ SampleProj/src/module-info.java
@@ -1,12 +1,7 @@
 module sampleProj {
   public class MyFirstJavaProgram {
-
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello India");
    }
 }
 }
