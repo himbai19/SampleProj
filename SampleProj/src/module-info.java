70c317151740c9d7b4bb2808270fe31a365546d8
Himanshu Bai <73933168+himbai19@users.noreply.github.com>
2021-10-28 07:52:18 +0530

    Added a hello world code

--- SampleProj/src/module-info.java
+++ SampleProj/src/module-info.java
@@ -1,2 +1,12 @@
 module sampleProj {
+  public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
 }
